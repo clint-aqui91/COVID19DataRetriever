fa0cbab1108ee36089cc376d19dce3f6066dbead
Clinton Aquilina <clinton.aquilina@outlook.com>
2022-09-15 09:57:29 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COVID19DataRetriever.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
