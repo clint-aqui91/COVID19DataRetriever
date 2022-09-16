bb47a76940e572e4092e6862da37ee411ac95a64
Clinton Aquilina <clinton.aquilina@outlook.com>
2022-09-16 13:01:03 +0200

    [FEAUTRE] Death Ratio calculation using JavaScript * JavaScript function to calculate death ratio in view was implemented.

--- wwwroot/js/functions.js
+++ wwwroot/js/functions.js
@@ -2,15 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-
-    <script type="text/javascript">
-
-        function CalculateDeathRatio(@Model.ConfirmedDeaths, @Model.ConfirmedCases)
-        {alert('You clicked ' + @Model.ConfirmedDeaths + "!");}
-
-    </script>
-
-
-
