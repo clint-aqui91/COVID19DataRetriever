378a113cf70e9d5b2577fc020feb1f25289c4dfd
Clinton Aquilina <clinton.aquilina@outlook.com>
2022-10-10 19:17:13 +0200

    [OTHER] Removed API key from web application's source code * This was done for security purposes. Furthermore, previous API key which was commited along with source code is also revoked from rapidapi.com

--- DataAccessLayer/DataAccess.cs
+++ DataAccessLayer/DataAccess.cs
@@ -4,7 +4,7 @@
     {
         /// <summary>
         /// Method <c>GetSerializedDataFromAPIAsync</c> is the method responsible of sending a request to the API and retrieving the returned response from the API's. Exception handling is also used here.
-        /// Replace the "-- Place API Key here -- with the API key acquired from RapidApi.com"
+        /// Replace the " -- Place API Key here -- " with the API key acquired from RapidApi.com
         /// </summary>
         public async Task<String> GetSerializedDataFromAPIAsync()
         {
@@ -17,7 +17,7 @@
                     RequestUri = new Uri("https://covid-19-statistics.p.rapidapi.com/reports?q=Malta&region_name=Malta&iso=MLT"),
                     Headers =
                 {
-                    { "X-RapidAPI-Key", "e286b44a29msh4c7a6448753438ap1f11a5jsn85743f38880b" },
+                    { "X-RapidAPI-Key", " -- Place API Key here -- " },
                     { "X-RapidAPI-Host", "covid-19-statistics.p.rapidapi.com" },
                 },
                 };
